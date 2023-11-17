8d9d78e84a227b13e043d14138635bf9c0fc97a2
Tyler Grissom <tylergrissom@me.com>
2023-11-17 10:08:48 -0600

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by Tyler Grissom on 11/17/23.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
